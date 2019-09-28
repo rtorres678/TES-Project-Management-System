6788ea818c925e8ecdd90d1f706efc3eb93932fc
Roberto Torres <roberto.torres@mavs.uta.edu>
2019-09-28 13:37:16 -0500

    add correct credentials for database

--- TES-PMS/test.js
+++ TES-PMS/test.js
@@ -2,15 +2,19 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
     host: "localhost",
-    user: "root",
+    user: "dev",
     password: "password"
 });
 
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
-    con.query("CREATE DATABASE mydb", function (err, result) {
+    /*con.query("CREATE DATABASE mydb1", function (err, result) {
         if (err) throw err;
         console.log("Database created");
+    });*/
+    con.query("SHOW DATABASES;", function (err, result) {
+        if (err) throw err;
+        console.log(result);
     });
 });
